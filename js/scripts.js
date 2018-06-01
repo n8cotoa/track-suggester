834854091c3cc01833cdf96ab5693f51f2c78cc8
Nate Cottle <cottle.nate@gmail.com>
2018-06-01 09:53:30 -0700

    Add functionality to the form reset button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
-function checkForm (){
+// business end
+function checkForm() {
 
 }
 
+function clearForm() {
+  $("#name").val("");
+  $("input[name=question1]").prop("checked", false);
+  $("input[name=question2]").prop("checked", false);
+  $("#question3").prop("selectedIndex", 0);
+  $("#question4").val("");
+  $("input[name=question5]").prop("checked", false);
+}
+// user end
 $(document).ready(function() {
   var question1 = $("input:radio[name=question1]:checked").val();
   var question2 = $("input:radio[name=question2]:checked").val();
@@ -9,9 +19,15 @@ $(document).ready(function() {
   var question4 = $("#question4").val();
   var question5 = $("input:radio[name=question5]:checked").val();
 
-  $("submit").click(function (e) {
+  $("#submit").click(function(e) {
     e.preventDefault();
 
     checkForm();
+  });
+
+  $("#reset").click(function(e) {
+    e.preventDefault();
+
+    clearForm();
   });
 });
