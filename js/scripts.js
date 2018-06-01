9e0c3efe58c4a1cd06284993c1b1c0fa137e65b8
Nate Cottle <cottle.nate@gmail.com>
2018-06-01 10:20:46 -0700

    add beginning of branch statement.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
 // business end
 function checkForm() {
+  var question1 = $("input:radio[name=question1]:checked").val();
+  var question2 = $("input:radio[name=question2]:checked").val();
+  var question3 = $("#question3").val();
+  var question4 = $("#question4").val();
+  var question5 = $("input:radio[name=question5]:checked").val();
 
+  if (question1 === "a" && question2 === "b" && question3 === "3" && (question4.length > 5) && question5 === "c") {
+    $("#result1").show();
+  // } else if {
+  //
+  // } else {
+
+  }
 }
 
 function clearForm() {
@@ -13,11 +25,11 @@ function clearForm() {
 }
 // user end
 $(document).ready(function() {
-  var question1 = $("input:radio[name=question1]:checked").val();
-  var question2 = $("input:radio[name=question2]:checked").val();
-  var question3 = $("#question3").val();
-  var question4 = $("#question4").val();
-  var question5 = $("input:radio[name=question5]:checked").val();
+  // var question1 = $("input:radio[name=question1]:checked").val();
+  // var question2 = $("input:radio[name=question2]:checked").val();
+  // var question3 = $("#question3").val();
+  // var question4 = $("#question4").val();
+  // var question5 = $("input:radio[name=question5]:checked").val();
 
   $("#submit").click(function(e) {
     e.preventDefault();
