b1803cd840ecc4b2f75ba093afb7aed167094b0b
Nate Cottle <cottle.nate@gmail.com>
2018-06-01 14:04:15 -0700

    Add content to form, change functionality of reset button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 // business end
-function randomResult() {
-  var resultArray = ["1", "2", "3"]
-  var resultNumber = resultArray[Math.floor(Math.random() * resultArray.length)];
+var resultArray = ["1", "2", "3"]
+var resultNumber = resultArray[Math.floor(Math.random() * resultArray.length)];
+
+function randomResult () {
   $("#result" + resultNumber).show();
   $('#result'+ resultNumber).scrollView();
   }
@@ -15,7 +16,7 @@ function randomResult() {
 }
 
 function checkForm() {
-  var name = $("#name").val("");
+  var name = $("#name").val();
   var question1 = $("input:radio[name=question1]:checked").val();
   var question2 = $("input:radio[name=question2]:checked").val();
   var question3 = $("#question3").val();
@@ -25,6 +26,7 @@ function checkForm() {
   if (question1 === "a" && question2 === "b" && question3 === "3" && (question4.length < 10) && question5 === "c") {
     $("#result1").show();
     $('#result1').scrollView();
+
   } else if (question1 === "b" && question2 ==="c" && question3 === "2" && (question4.length > 20) && question5 === "b") {
     $("#reseult2").show();
     $('#result2').scrollView();
@@ -33,17 +35,38 @@ function checkForm() {
     $('#result3').scrollView();
   } else {
     randomResult();
+    $("div#addName").prepend("<h4>Hey! " + name + ", you should try:</h4>");
   }
 }
 
-function clearForm() {
-  $("#name").val("");
-  $("input[name=question1]").prop("checked", false);
-  $("input[name=question2]").prop("checked", false);
-  $("#question3").prop("selectedIndex", 0);
-  $("#question4").val("");
-  $("input[name=question5]").prop("checked", false);
+function inputHasValues() {
+  if ( $("#name").val() && $("input:radio[name=question1]").val() && $("input:radio[name=question2]").val() && $("#question3").val() && $("#question4").val() && $("input:radio[name=question5]").val() ) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+function disableAllBtns() {
+  $("#submit").prop('disabled', 'true');
+}
+
+function enableAllBtns() {
+  $("#submit").removeAttr("disabled");
+}
+
+
+// function clearForm() {
+//   $("#name").val("");
+//   $("input[name=question1]").prop("checked", false);
+//   $("input[name=question2]").prop("checked", false);
+//   $("#question3").prop("selectedIndex", 0);
+//   $("#question4").val("");
+//   $("input[name=question5]").prop("checked", false);
+//
+//   $("#result" + resultNumber).hide();
+//   $("h4#addName").hide();
+// }
 
 // user end
 $(document).ready(function() {
@@ -52,17 +75,23 @@ $(document).ready(function() {
   // var question3 = $("#question3").val();
   // var question4 = $("#question4").val();
   // var question5 = $("input:radio[name=question5]:checked").val();
+  disableAllBtns();
+
+  $("#name, input[name=question1], input[name=question2], #question3, #question4, input[name=question5]").keyup(function() {
+    inputHasValues() ? enableAllBtns() : disableAllBtns();
+  });
 
   $("#submit").click(function(e) {
     e.preventDefault();
 
     checkForm();
+
   });
 
   $("#reset").click(function(e) {
     e.preventDefault();
-
-    clearForm();
+    location.reload();
+    // clearForm();
   });
 
 
