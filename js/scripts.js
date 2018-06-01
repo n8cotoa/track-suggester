2794f8159d67c4befa43807f0cdde600c7b0f6cd
Nate Cottle <cottle.nate@gmail.com>
2018-06-01 09:43:44 -0700

    add results to html file, variables for questions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
+function checkForm (){
 
+}
+
+$(document).ready(function() {
+  var question1 = $("input:radio[name=question1]:checked").val();
+  var question2 = $("input:radio[name=question2]:checked").val();
+  var question3 = $("#question3").val();
+  var question4 = $("#question4").val();
+  var question5 = $("input:radio[name=question5]:checked").val();
+
+  $("submit").click(function (e) {
+    e.preventDefault();
+
+    checkForm();
+  });
+});
