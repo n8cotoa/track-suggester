73f116803bda0d6f0ecb4d9a75a4e7fd26d3dec4
Nate Cottle <cottle.nate@gmail.com>
2018-06-01 13:01:43 -0700

    Add scroll to result function.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,19 @@ function randomResult() {
   var resultArray = ["1", "2", "3"]
   var resultNumber = resultArray[Math.floor(Math.random() * resultArray.length)];
   $("#result" + resultNumber).show();
+  $('#result'+ resultNumber).scrollView();
   }
 
-function checkForm() {
+  $.fn.scrollView = function () {
+  return this.each(function () {
+    $('html, body').animate({
+      scrollTop: $(this).offset().top
+    }, 1000);
+  });
+}
 
+function checkForm() {
+  var name = $("#name").val("");
   var question1 = $("input:radio[name=question1]:checked").val();
   var question2 = $("input:radio[name=question2]:checked").val();
   var question3 = $("#question3").val();
@@ -15,10 +24,13 @@ function checkForm() {
 
   if (question1 === "a" && question2 === "b" && question3 === "3" && (question4.length < 10) && question5 === "c") {
     $("#result1").show();
+    $('#result1').scrollView();
   } else if (question1 === "b" && question2 ==="c" && question3 === "2" && (question4.length > 20) && question5 === "b") {
     $("#reseult2").show();
+    $('#result2').scrollView();
   } else if (question1 === "c" && question2 === "a" && question3 === "1" && question5 === "a") {
     $("#result3").show();
+    $('#result3').scrollView();
   } else {
     randomResult();
   }
@@ -32,6 +44,7 @@ function clearForm() {
   $("#question4").val("");
   $("input[name=question5]").prop("checked", false);
 }
+
 // user end
 $(document).ready(function() {
   // var question1 = $("input:radio[name=question1]:checked").val();
@@ -51,4 +64,6 @@ $(document).ready(function() {
 
     clearForm();
   });
+
+
 });
